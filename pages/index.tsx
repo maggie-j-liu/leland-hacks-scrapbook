31a1663587597484d0167ca8b2a48f447ceeda60
Maggie Liu <63619830+maggie-j-liu@users.noreply.github.com>
2022-09-03 22:06:24 +0000

    add prisma and next-auth

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,18 @@
+import { useSession, signIn, signOut } from "next-auth/react";
 export default function Home() {
+  const { data: session } = useSession();
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user?.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    );
+  }
   return (
-    <main>hello world!</main>
+    <>
+      Not signed in <br />
+      <button onClick={() => signIn()}>Sign in</button>
+    </>
   );
 }
